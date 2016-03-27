5fa6b5aa5653f917504987686ab84b9daa08bfd3
qnope <antoine.morrier@telecom-sudparis.eu>
2016-03-27 18:44:51 +0200

    Readme

--- System/noncopyable.hpp
+++ System/noncopyable.hpp
@@ -5,4 +5,9 @@ public:
     NonCopyable() = default;
     NonCopyable(NonCopyable const &) = delete;
     NonCopyable &operator=(NonCopyable const &) = delete;
+
+    // I am to lazy to implement them right now
+    NonCopyable(NonCopyable&&) = delete;
+    NonCopyable &operator =(NonCopyable&&) = delete;
+
 };
